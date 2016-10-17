65e03b588cd53126c2fdff59d32a198a97dcf225
Ahmed Soliman <ahmedanwar@mail.com>
2016-10-16 22:42:58 -0700

    fixed the errore of the array ingredients

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../recipe';
+
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
    recipes: Recipe[] = [
-     new Recipe('Greek Salad', 'Very tasty', 'http://previews.123rf.com/images/serezniy/serezniy1110/serezniy111000532/10817766-Deliciosa-ensalada-griega-en-la-placa-aislada-en-blanco-Foto-de-archivo.jpg', '[]');
-     new Recipe('Summer Salad', 'Nice and Sunny', 'http://homecaprice.com/wp-content/uploads/2015/05/3-salad.jpg', '[]');
+     new Recipe('Greek Salad', 'Very tasty', 'http://previews.123rf.com/images/serezniy/serezniy1110/serezniy111000532/10817766-Deliciosa-ensalada-griega-en-la-placa-aislada-en-blanco-Foto-de-archivo.jpg', []),
+     new Recipe('Summer Salad', 'Nice and Sunny', 'http://homecaprice.com/wp-content/uploads/2015/05/3-salad.jpg', [])
    ];
    @Output() recipeSelected = new EventEmitter<Recipe>()
   
